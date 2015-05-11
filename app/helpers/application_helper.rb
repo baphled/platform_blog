944af4e267f29b5db45f1ff58520466dd2ec8699
Yomi Colledge <baphled@boodah.net>
2015-05-11 10:20:14 +0100

    Display articles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def markdown(content)
+    options = {
+      autolink: true, 
+      space_after_headers: true, 
+      fenced_code_blocks: true,
+      underline: true,
+      highlight: true,
+      footnotes: true,
+      tables: true
+    }
+
+    @markdown ||= Redcarpet::Markdown.new(BlogMarkdownRenderer, options)
+    @markdown.render(content)
+  end
 end
